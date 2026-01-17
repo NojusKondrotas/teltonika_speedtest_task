35dac095ef1a4c8b376811afadac1d2912604c6d
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-01-17 15:01:13 +0300

    Declare functions for server list filtering

--- src/server/server.h
+++ src/server/server.h
@@ -12,5 +12,7 @@ typedef struct s_server {
 }Server;
 
 Server *load_servers(const char *filepath, size_t *count);
+Server *get_servers_by_city(Server *servers, size_t n);
+Server *get_servers_by_country(Server *servers, size_t n);
 
 #endif
