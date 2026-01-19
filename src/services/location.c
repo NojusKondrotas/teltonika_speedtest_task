5f2b4c531f688969388f04c521aa0fb67efff060
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-01-19 11:33:15 +0300

    Implement location finder function

--- src/services/location.c
+++ src/services/location.c
@@ -1,3 +1,4 @@
+#include "../cJSON/cJSON.h"
 #include "location.h"
 
 #include <curl/curl.h>
@@ -22,8 +23,78 @@ static size_t write_cb(void *ptr, size_t size, size_t nmemb, void *userdata) {
     return total;
 }
 
-int get_user_location(char *ip) {
+int get_user_location(char *ip, char **city, char **country) {
+    if(!city || !country) {
+        return EXIT_FAILURE;
+    }
+    *city = NULL;
+    *country = NULL;
+
+    char url[64];
+    if(ip && ip[0] != '\0') {
+        snprintf(url, sizeof(url), "http://ip-api.com/json/%s", ip);
+    } else {
+        snprintf(url, sizeof(url), "http://ip-api.com/json/");
+    }
+
+    CURL *curl = curl_easy_init();
+    if(!curl) {
+        fprintf(stderr, "curl init failed\n");
+        return EXIT_FAILURE;
+    }
+
+    char *response;
+
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_cb);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+    CURLcode res = curl_easy_perform(curl);
+    if(res != CURLE_OK) {
+        fprintf(stderr, "curl encountered an error: %s\n", curl_easy_strerror(res));
+        free(response);
+        curl_easy_cleanup(curl);
+        return EXIT_FAILURE;
+    }
+    curl_easy_cleanup(curl);
     
+    cJSON *json = cJSON_Parse(response);
+    free(response);
+
+    if(!json) {
+        fprintf(stderr, "Failure parsing JSON\n");
+        return EXIT_FAILURE;
+    }
+
+    cJSON *status = cJSON_GetObjectItem(json, "status");
+    if(!status || strcmp(status->valuestring, "success") != 0) {
+        fprintf(stderr, "Not able to get response status\n");
+        cJSON_Delete(json);
+        return EXIT_FAILURE;
+    }
+
+    cJSON *city_json = cJSON_GetObjectItem(json, "city");
+    cJSON *country_json = cJSON_GetObjectItem(json, "country");
+
+    if(city_json && cJSON_IsString(city_json) && city_json->valuestring) {
+        *city = strdup(city_json->valuestring);
+    } else {
+        fprintf(stderr, "Not able to geolocate city\n");
+        cJSON_Delete(json);
+        return EXIT_FAILURE;
+    }
+
+    if(country_json && cJSON_IsString(country_json) && country_json->valuestring) {
+        *country = strdup(country_json->valuestring);
+    } else {
+        fprintf(stderr, "Not able to geolocate country\n");
+        free(*city);
+        cJSON_Delete(json);
+        return EXIT_FAILURE;
+    }
+
+    cJSON_Delete(json);
+    return EXIT_SUCCESS;
 }
 
 int get_user_ip(char **ip) {
--- src/services/location.h
+++ src/services/location.h
@@ -1,7 +1,7 @@
 #ifndef LOCATION_H
 #define LOCATION_H
 
-int get_user_location(char *ip);
+int get_user_location(char *ip, char **city, char **country);
 int get_user_ip(char **ip);
 
 #endif
