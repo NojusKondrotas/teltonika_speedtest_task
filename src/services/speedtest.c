ee415276000432c9eb990d6e33900c32262a95d1
NojusKondrotas <nojus.kondrotas23@gmail.com>
2026-01-19 20:32:30 +0300

    Define function for http header appending

--- src/services/speedtest.c
+++ src/services/speedtest.c
@@ -4,6 +4,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct curl_slist *add_headers(CURL *curl) {
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0");
+    headers = curl_slist_append(headers, "Content-Type: application/octet-stream");
+    headers = curl_slist_append(headers, "Accept: */*");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+    return headers;
+}
+
 int get_download_speed(Server *servers, size_t count) {
     return EXIT_SUCCESS;
 }
--- src/services/speedtest.h
+++ src/services/speedtest.h
@@ -3,6 +3,7 @@
 
 #include "../server/server.h"
 
+#include <curl/curl.h>
 #include <stdlib.h>
 
 #define UP_BUFFER_SIZE (100 * 1024 * 1024)
@@ -11,6 +12,7 @@ typedef struct s_upload_data{
     char *buffer[UP_BUFFER_SIZE];
 }UploadData;
 
+struct curl_slist *add_headers(CURL *curl);
 int get_download_speed(Server *servers, size_t count);
 int get_upload_speed(Server *servers, size_t count);
 
